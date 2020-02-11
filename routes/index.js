9c5adcf78e49f26cfb30b3c479537a91c7629bd6
yrpatel95 <58954825+yrpatel95@users.noreply.github.com>
2020-02-10 19:23:16 -0500

    added homepage

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ const {ensureAuthenticated} = require('../config/auth');
 // Welcome Page
 router.get('/', (req, res) => res.render('welcome'));
 
+// Home Page
+router.get('/home', (req, res) => res.render('home'));
+
 // Dashboard Page
 router.get('/dashboard', ensureAuthenticated, (req, res) => res.render('dashboard', {
     name: req.user.name
